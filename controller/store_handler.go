9778e1f72900561882c9ee06ba3483e80fc6b4ea
Vixens <kd1175173@st.kobedenshi.ac.jp>
2020-01-17 11:02:41 +0900

    actualizing auth_google

--- controller/store_handler.go
+++ controller/store_handler.go
@@ -1,16 +1,18 @@
 package controller
 
 import (
+	"CRUD_APP/model"
+	"image/png"
+	"math/rand"
 	"net/http"
+	"os"
 	"strconv"
+	"time"
 
-	// "image/png"
-	"crud_go/model"
-
+	"github.com/boombuler/barcode"
+	"github.com/boombuler/barcode/qr"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
-	// "github.com/boombuler/barcode"
-	// "github.com/boombuler/barcode/qr"
 )
 
 type Server struct {
@@ -21,17 +23,58 @@ func (s *Server) GetStorePage(c *gin.Context) {
 	errMsg := ""
 	stores, err := model.GetAllStores(s.DB)
 	if err != nil {
-		errMsg = "エラー発生"
+		errMsg = "indexページ取得失敗"
 		stores = []model.Store{}
 	}
-	c.HTML(http.StatusOK, "index.tmpl", gin.H{
+	c.HTML(http.StatusOK, "index.html", gin.H{
 		"stores": stores,
 		"errMsg": &errMsg,
 	})
 }
 
+func (s *Server) GetDetailPage(c *gin.Context) {
+	errMsg := ""
+	ID, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.Redirect(http.StatusMovedPermanently, "/")
+		return
+	}
+	store, err := model.GetStoreByID(s.DB, uint(ID))
+	if err != nil {
+		errMsg = "詳細ページ取得失敗"
+		store = &model.Store{}
+	}
+	c.HTML(http.StatusOK, "detail.html", gin.H{
+		"store":  store,
+		"errMsg": &errMsg,
+	})
+
+}
+
+func (s *Server) CreateQRHandler(c *gin.Context) {
+
+	rand.Seed(time.Now().UnixNano())
+	qr_var_random := rand.Float64()
+	formated_var_qr := strconv.FormatFloat(qr_var_random, 'f', 4, 64)
+
+	// Create the barcode
+	qrCode, _ := qr.Encode(formated_var_qr, qr.M, qr.Auto)
+
+	// Scale the barcode to 200x200 pixels
+	qrCode, _ = barcode.Scale(qrCode, 200, 200)
+
+	// create the output file
+	file, _ := os.Create("qrcode.png")
+	defer file.Close()
+
+	// encode the barcode as png
+	png.Encode(file, qrCode)
+	c.Redirect(http.StatusMovedPermanently, "/detail/:id")
+
+}
+
 func (s *Server) NewStorePage(c *gin.Context) {
-	c.HTML(http.StatusOK, "new.tmpl", gin.H{})
+	c.HTML(http.StatusOK, "new.html", gin.H{})
 }
 
 func (s *Server) CreateStoreHandler(c *gin.Context) {
@@ -46,38 +89,38 @@ func (s *Server) CreateStoreHandler(c *gin.Context) {
 	store.Tables = c.Request.Form["Tables"][0]
 
 	if store.Storename == "" {
-		c.HTML(http.StatusBadRequest, "new.tmpl", gin.H{
+		c.HTML(http.StatusBadRequest, "new.html", gin.H{
 			"errMsg": "empty:Storename",
 		})
 		return
 	} else if store.Loc == "" {
-		c.HTML(http.StatusBadRequest, "new.tmpl", gin.H{
+		c.HTML(http.StatusBadRequest, "new.html", gin.H{
 			"errMsg": "empty:Address",
 		})
 		return
 	} else if store.Genre == "" {
-		c.HTML(http.StatusBadRequest, "new.tmpl", gin.H{
+		c.HTML(http.StatusBadRequest, "new.html", gin.H{
 			"errMsg": "empty:Genre",
 		})
 		return
 	} else if store.Tel == "" {
-		c.HTML(http.StatusBadRequest, "new.tmpl", gin.H{
+		c.HTML(http.StatusBadRequest, "new.html", gin.H{
 			"errMsg": "empty:Tel",
 		})
 		return
 	} else if store.Information == "" {
-		c.HTML(http.StatusBadRequest, "new.tmpl", gin.H{
+		c.HTML(http.StatusBadRequest, "new.html", gin.H{
 			"errMsg": "empty:Information",
 		})
 		return
 	} else if store.Tables == "" {
-		c.HTML(http.StatusBadRequest, "new.tmpl", gin.H{
+		c.HTML(http.StatusBadRequest, "new.html", gin.H{
 			"errMsg": "empty:Tables",
 		})
 		return
 	}
 	if err := model.CreateStore(s.DB, store); err != nil {
-		c.HTML(http.StatusBadRequest, "new.tmpl", gin.H{
+		c.HTML(http.StatusBadRequest, "new.html", gin.H{
 			"errMsg": "登録できませんでした	",
 		})
 		return
@@ -86,14 +129,14 @@ func (s *Server) CreateStoreHandler(c *gin.Context) {
 }
 
 func (s *Server) DeleteStoreHandler(c *gin.Context) {
-	StoreID, err := strconv.Atoi(c.Param("id"))
+	ID, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
 		c.Redirect(http.StatusMovedPermanently, "/")
 		return
 	}
-	if err := model.DeleteStoreByID(s.DB, uint(StoreID)); err != nil {
+	if err := model.DeleteStoreByID(s.DB, uint(ID)); err != nil {
 		stores, _ := model.GetAllStores(s.DB)
-		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+		c.HTML(http.StatusOK, "index.html", gin.H{
 			"stores": stores,
 			"errMsg": "データ削除に失敗しました。",
 		})
--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"crud_go/controller"
-	"crud_go/util"
+	"CRUD_APP/controller"
+	"CRUD_APP/util"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -15,12 +16,30 @@ func main() {
 	router := gin.Default()
 
 	router.LoadHTMLGlob("view/*")
+	http.HandleFunc("/", moveHandler)
+	// router.GET("/", server.GetStorePage)
 
-	router.GET("/", server.GetStorePage)
+	/* /loginへのハンドラ(認証プロバイダの選択) */
+	http.Handle("/login", &templateHandler{filename: "/login.html"})
+
+	/* 認証用のgomniauthの初期設定 */
+	setAuthInfo()
+
+	/* /authへのハンドラ(指定プロバイダでの認証) */
+	http.HandleFunc("/auth/", authHandler)
+
+	/* /afterへのハンドラ(認証後のページ・サーブ) */
+	http.Handle("/after", &templateHandler{filename: "/after.html"})
+
+	/* /logoutへのハンドラ(認証情報の削除) */
+	http.HandleFunc("/logout", logoutHandler)
+
 	router.GET("/new", server.NewStorePage)
+	router.GET("/detail/:id", server.GetDetailPage)
 
 	router.POST("/new", server.CreateStoreHandler)
 	router.POST("/delete/:id", server.DeleteStoreHandler)
+	router.POST("/detail/:id", server.CreateQRHandler)
 
 	router.Run(":8080")
 }
